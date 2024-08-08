c191f4c8ad2046c73538a73ba872e2e35d5611c2
ishaC02 <isha@usedatabrain.com>
2024-08-08 15:51:14 +0530

    feat: add server event in angular

--- dbn-demo-angular/src/app/app.module.ts
+++ dbn-demo-angular/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+(window as any).databrainServerEvent = (error: any) => {
+  console.log(error);
+};
+
 @NgModule({
   declarations: [
     AppComponent
